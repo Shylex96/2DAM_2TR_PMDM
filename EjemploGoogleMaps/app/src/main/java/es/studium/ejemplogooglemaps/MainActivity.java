715eb39216924439fcbb4982002a6878f2fa5fb8
Shylex96 <estebanalbarran1996@gmail.com>
2024-01-22 10:48:38 +0100

    google maps

---
+++ EjemploGoogleMaps/app/src/main/java/es/studium/ejemplogooglemaps/MainActivity.java
@@ -0,0 +1,73 @@
+package es.studium.ejemplogooglemaps;
+
+import androidx.core.app.ActivityCompat;
+import androidx.fragment.app.FragmentActivity;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.view.View;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback,GoogleMap.OnMapClickListener
+{
+    private GoogleMap mapa;
+    private final LatLng grupoStudium = new LatLng(37.39680717426016,
+            -5.972389626774901);
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
+        assert mapFragment != null;
+        mapFragment.getMapAsync(this);
+    }
+    @Override public void onMapReady(GoogleMap googleMap)
+    {
+        mapa = googleMap;
+        mapa.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+        mapa.getUiSettings().setZoomControlsEnabled(false);
+        mapa.moveCamera(CameraUpdateFactory.newLatLngZoom(grupoStudium, 15));
+        mapa.addMarker(new MarkerOptions()
+                .position(grupoStudium)
+                .title("Grupo Studium")
+                .snippet("Instituto Técnico Superior de Informática de Sevilla S.L.")
+                .icon(BitmapDescriptorFactory
+                        .fromResource(android.R.drawable.ic_menu_compass))
+                .anchor(0.5f, 0.5f));
+        mapa.setOnMapClickListener(this);
+        if (ActivityCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED)
+        {
+            mapa.setMyLocationEnabled(true);
+            mapa.getUiSettings().setCompassEnabled(true);
+        }
+    }
+    public void moveCamera(View view)
+    {
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(grupoStudium));
+    }
+    public void animateCamera(View view)
+    {
+        mapa.animateCamera(CameraUpdateFactory.newLatLng(grupoStudium));
+    }
+    public void addMarker(View view)
+    {
+        mapa.addMarker(new MarkerOptions().position(
+                mapa.getCameraPosition().target));
+    }
+    @Override public void onMapClick(LatLng puntoPulsado)
+    {
+        mapa.addMarker(new MarkerOptions().position(puntoPulsado)
+                .icon(BitmapDescriptorFactory
+                        .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+}
